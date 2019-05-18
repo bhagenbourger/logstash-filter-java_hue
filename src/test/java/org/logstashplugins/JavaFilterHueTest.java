27623bb0aa77248a239292680b32180a9d69b3c1
benoit <benoit@hagenbourger.fr>
2019-05-18 14:44:47 +0200

    Create filter from logstash-filter-java_filter_example

--- src/test/java/org/logstashplugins/JavaFilterHueTest.java
+++ src/test/java/org/logstashplugins/JavaFilterHueTest.java
@@ -13,14 +13,14 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class JavaFilterExampleTest {
+public class JavaFilterHueTest {
 
     @Test
     public void testJavaExampleFilter() {
         String sourceField = "foo";
         Configuration config = new ConfigurationImpl(Collections.singletonMap("source", sourceField));
         Context context = new ContextImpl(null);
-        JavaFilterExample filter = new JavaFilterExample("test-id", config, context);
+        JavaFilterHue filter = new JavaFilterHue("test-id", config, context);
 
         Event e = new org.logstash.Event();
         TestMatchListener matchListener = new TestMatchListener();
