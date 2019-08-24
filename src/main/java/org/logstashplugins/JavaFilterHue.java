eab1aff08421a0217c3896cb163550bba4041d5a
benoit <benoit@hagenbourger.fr>
2019-08-24 12:37:27 +0200

    Handle temperature sensors

--- src/main/java/org/logstashplugins/JavaFilterHue.java
+++ src/main/java/org/logstashplugins/JavaFilterHue.java
@@ -2,39 +2,51 @@ package org.logstashplugins;
 
 import co.elastic.logstash.api.*;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static co.elastic.logstash.api.PluginConfigSpec.stringSetting;
 
 // class name must match plugin name
 @LogstashPlugin(name = "java_filter_hue")
 public class JavaFilterHue implements Filter {
 
-    private static final PluginConfigSpec<String> SOURCE_CONFIG =
-            PluginConfigSpec.stringSetting("lights", "lights");
+    static final String HUE_SOURCE_FIELD_METADATA = "HUE_SOURCE_FIELD_METADATA";
+    static final String LIGHTS = "lights";
+    static final String SENSORS = "sensors";
+    private static final String TYPE_FIELD = "type";
+    private static final String TEMPERATURE_TYPE = "ZLLTemperature";
+    private static final PluginConfigSpec<String> LIGHTS_SOURCE_CONFIG = stringSetting(LIGHTS, LIGHTS);
+    private static final PluginConfigSpec<String> SENSORS_SOURCE_CONFIG = stringSetting(SENSORS, SENSORS);
+    private static final Predicate<? super Event> LIGHTS_PREDICATE = e -> true;
+    // Keep only temperature sensors
+    private static final Predicate<? super Event> SENSORS_PREDICATE = e -> TEMPERATURE_TYPE.equals(e.getField(TYPE_FIELD));
 
     private String id;
-    private String sourceField;
+    private String lightsSourceField;
+    private String sensorsSourceField;
 
     public JavaFilterHue(String id, Configuration config, Context context) {
         // constructors should validate configuration options
         this.id = id;
-        this.sourceField = config.get(SOURCE_CONFIG);
+        this.lightsSourceField = config.get(LIGHTS_SOURCE_CONFIG);
+        this.sensorsSourceField = config.get(SENSORS_SOURCE_CONFIG);
     }
 
     @Override
     public Collection<Event> filter(Collection<Event> events, FilterMatchListener matchListener) {
 
         return events.stream()
-                .flatMap(e -> generateLightEvents(e, matchListener).stream())
+                .flatMap(e -> generateAllEvents(e, matchListener))
                 .collect(Collectors.toSet());
     }
 
     @Override
     public Collection<PluginConfigSpec<?>> configSchema() {
         // should return a list of all configuration options for this plugin
-        return Collections.singletonList(SOURCE_CONFIG);
+        return Arrays.asList(LIGHTS_SOURCE_CONFIG, SENSORS_SOURCE_CONFIG);
     }
 
     @Override
@@ -42,17 +54,42 @@ public class JavaFilterHue implements Filter {
         return this.id;
     }
 
-    private Collection<Event> generateLightEvents(Event event, FilterMatchListener matchListener) {
-        return ((Map<String, Map<String, Object>>) event.getField(sourceField)).values().stream()
-                .map(l -> generateLightEvent(event, l, matchListener))
+    private Stream<Event> generateAllEvents(Event event, FilterMatchListener matchListener) {
+        return Stream.concat(
+                generateEvents(event, matchListener, lightsSourceField, LIGHTS_PREDICATE).stream(),
+                generateEvents(event, matchListener, sensorsSourceField, SENSORS_PREDICATE).stream()
+        );
+    }
+
+    private Collection<Event> generateEvents(
+            Event event,
+            FilterMatchListener matchListener,
+            String sourceField,
+            Predicate<? super Event> predicate
+    ) {
+        return getMapData(event, sourceField).values().stream()
+                .map(d -> generateEvent(event, d, sourceField))
+                .filter(predicate)
+                .peek(e -> matchListener.filterMatched(event))
                 .collect(Collectors.toSet());
     }
 
-    private Event generateLightEvent(Event event, Map<String, Object> light, FilterMatchListener matchListener) {
-        final org.logstash.Event lightEvent = new org.logstash.Event(light);
-        lightEvent.setEventTimestamp(event.getEventTimestamp());
-        lightEvent.setField(org.logstash.Event.METADATA, event.getMetadata());
-        matchListener.filterMatched(lightEvent);
-        return lightEvent;
+    private static Event generateEvent(Event originalEvent, Map<String, Object> data, String sourceField) {
+        // Keep original timestamp
+        final org.logstash.Event event = new org.logstash.Event(data);
+        event.setEventTimestamp(originalEvent.getEventTimestamp());
+
+        // Keep original metadata and add HUE_SOURCE_FIELD_METADATA metadata
+        final Map<String, Object> metadata = originalEvent.getMetadata();
+        metadata.put(HUE_SOURCE_FIELD_METADATA, sourceField);
+        event.setField(org.logstash.Event.METADATA, metadata);
+
+        return event;
+    }
+
+    private static Map<String, Map<String, Object>> getMapData(Event event, String sourceFiled) {
+        return Optional
+                .ofNullable((Map<String, Map<String, Object>>) event.getField(sourceFiled))
+                .orElse(Collections.emptyMap());
     }
 }
--- src/test/java/org/logstashplugins/JavaFilterHueTest.java
+++ src/test/java/org/logstashplugins/JavaFilterHueTest.java
@@ -6,6 +6,7 @@ import co.elastic.logstash.api.Event;
 import co.elastic.logstash.api.FilterMatchListener;
 import com.google.gson.Gson;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.logstash.plugins.ConfigurationImpl;
 import org.logstash.plugins.ContextImpl;
@@ -14,46 +15,85 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
+
+import static org.logstashplugins.JavaFilterHue.*;
 
 public class JavaFilterHueTest {
 
     private static final String DATA_FILE = "data.json";
 
+    private Event event = generateEvent();
+    private TestMatchListener matchListener;
+
+    @Before
+    public void setUp() {
+        matchListener = new TestMatchListener();
+    }
+
     @Test
-    public void shouldSplitEachLightInEvent() throws IOException {
-        final String sourceField = "lights";
-        final Configuration config = new ConfigurationImpl(Collections.singletonMap("source", sourceField));
+    public void shouldSplitEachLightAndEachTemperatureSensorInEvent() {
+        final Configuration config = new ConfigurationImpl(Collections.emptyMap());
         final Context context = new ContextImpl(null, null);
         final JavaFilterHue filter = new JavaFilterHue("test-id", config, context);
-        final Event event = generateEvent();
-        final TestMatchListener matchListener = new TestMatchListener();
 
         final Collection<Event> results = filter.filter(Collections.singleton(event), matchListener);
 
-        // Lights are split into several events
-        Assert.assertEquals(5, results.size());
-        Assert.assertEquals(5, matchListener.getMatchCount());
+        // Lights and temperature sensors are split into several events
+        Assert.assertEquals(7, results.size());
+        Assert.assertEquals(7, matchListener.getMatchCount());
+
+        // Should keep 5 lights
+        Assert.assertEquals(5, count(results, LIGHTS));
+
+        // Should keep 2 temperature sensors
+        Assert.assertEquals(2, count(results, SENSORS));
 
         // Original timestamp is kept
-        Assert.assertTrue(results.stream().allMatch(e -> e.getEventTimestamp().equals(event.getEventTimestamp())));
+        assertOriginalTimestampIsKept(results);
 
         // Original metadata are kept
+        assertOriginalMetadataAreKept(results);
+
+    }
+
+    @Test
+    public void shouldNotFindLightsAndTemperatureSensorWhenConfigIsWrong() {
+        final Map<String, Object> configData = new HashMap<>();
+        configData.put(LIGHTS, "wrong");
+        configData.put(SENSORS, "bad");
+        final Configuration config = new ConfigurationImpl(configData);
+        final Context context = new ContextImpl(null, null);
+        final JavaFilterHue filter = new JavaFilterHue("test-id", config, context);
+
+        final Collection<Event> results = filter.filter(Collections.singleton(event), matchListener);
+
+        Assert.assertTrue(results.isEmpty());
+    }
+
+    private void assertOriginalTimestampIsKept(Collection<Event> results) {
+        Assert.assertTrue(results.stream().allMatch(e -> e.getEventTimestamp().equals(event.getEventTimestamp())));
+    }
+
+    private void assertOriginalMetadataAreKept(Collection<Event> results) {
+        final Predicate<? super Map.Entry<String, Object>> excludeHueSourceFieldMetadate = m -> !HUE_SOURCE_FIELD_METADATA.equals(m.getKey());
         Assert.assertTrue(
                 results.stream().allMatch(
-                        e -> event.getMetadata().entrySet().stream().allMatch(
+                        e -> event.getMetadata().entrySet().stream().filter(excludeHueSourceFieldMetadate).allMatch(
                                 m -> e.getMetadata().get(m.getKey()).equals(m.getValue())))
         );
     }
 
-    private Event generateEvent() throws IOException {
-        final org.logstash.Event event = new org.logstash.Event(jsonToMap(getJsonData(getDataFile())));
-        event.setField(org.logstash.Event.METADATA, generateMetadata());
-        return event;
+    private Event generateEvent() {
+        try {
+            final org.logstash.Event event = new org.logstash.Event(jsonToMap(getJsonData(getDataFile())));
+            event.setField(org.logstash.Event.METADATA, generateMetadata());
+            return event;
+        } catch (IOException e) {
+            return null;
+        }
     }
 
     private String getDataFile() {
@@ -86,6 +126,12 @@ public class JavaFilterHueTest {
     private static Map<? extends Serializable, Object> jsonToMap(String json) {
         final Gson gson = new Gson();
         return gson.fromJson(json, Map.class);
+    }
+
+    private static long count(Collection<Event> results, String hueSourceFieldMetadataValue) {
+        return results.stream()
+                .filter(e -> hueSourceFieldMetadataValue.equals(e.getMetadata().get(HUE_SOURCE_FIELD_METADATA).toString()))
+                .count();
     }
 }
 
